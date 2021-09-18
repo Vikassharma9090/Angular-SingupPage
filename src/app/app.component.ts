a717cf5b6ae2a61cc48713dddc0193b2ea443dd4
Vikas Sharma <vikaskumarasharma06470@gmail.com>
2021-09-19 02:15:24 +0530

    Changes Something

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Vikas Sharma First Angular App';
+  // title = 'Vikas Sharma First Angular App';
+  //  Ternary Operator Angular Interpolation
+  // title='';
+  // video:string='plz scubscribe';
+  // like:string='share with Social Media';
+
+  // Function in Angular
+  firstname:string='vikassharma';
+  lastname:string='Offical';
+
+  Something():string{
+    return this.firstname+this.lastname
+  }
+
 }
